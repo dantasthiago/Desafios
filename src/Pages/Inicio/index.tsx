78d31d4fd251473b656d92f34da810c8e39cd4f8
dantasthiago <thiago_dantas88@hotmail.com>
2022-06-03 09:13:50 -0300

    inclusao barra de busca

--- src/Pages/Inicio/index.tsx
+++ src/Pages/Inicio/index.tsx
@@ -1,6 +1,5 @@
 import BannerComplete from "../../assets/BannerComplete.png";
 import styles from "./Inicio.module.scss";
-import NavBar from "../../components/NavBar";
 import { useNavigate } from 'react-router-dom';
 
 export default function Inicio() {
--- src/Pages/Pokemons/index.tsx
+++ src/Pages/Pokemons/index.tsx
@@ -1,11 +1,15 @@
-import NavBar from '../../components/NavBar'
-import styles from './Pokemons.module.scss'
+import { useState } from "react";
+import Buscador from "./Buscador";
+import styles from "./Pokemons.module.scss";
 
-export default function Pokemons(){
-  return(
-    <div>
-      Pokemons
-
-    </div>
-  )
+export default function Pokemons() {
+	const [busca, setBusca] = useState("");
+	return (
+		<>
+			<div className={styles.texto}>
+				Mais de 250 Pokemons para você escolher o seu favorito
+			</div>
+			<Buscador busca={busca} setBusca={setBusca} />
+		</>
+	);
 }
